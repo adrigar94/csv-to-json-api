634dd5dfbc27cd9c2caeb6491604a9b0565912b9
Adrián García <akrian231@gmail.com>
2022-09-12 18:32:42 +0200

    data provider in test

--- tests/CsvToJson/CsvToJsonTest.php
+++ tests/CsvToJson/CsvToJsonTest.php
@@ -7,34 +7,30 @@ use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 
 class CsvToJsonTest extends KernelTestCase
 {
-
-    public function testSomething()
+    public function csvProvider(): array
     {
-        $this->it_should_parse_csv_to_json();
-        $this->it_should_parse_string_num_to_ints();
+        return [
+            [
+                "col1;col2\nval1.1;val1.2\nval2.1;val2.2",
+                '[{"col1":"val1.1","col2":"val1.2"},{"col1":"val2.1","col2":"val2.2"}]'
+            ],
+            [
+                "col1;col2\n1;2\n1.1;2.2",
+                '[{"col1":1,"col2":2},{"col1":1.1,"col2":2.2}]'
+            ],
+        ];
     }
 
-    private function it_should_parse_csv_to_json()
-    {
-        $csvToParse = "col1;col2\nval1.1;val1.2\nval2.1;val2.2";
 
+    /**
+     * @dataProvider csvProvider
+     */
+    public function test_it_should_parse_csv_to_json($csvToParse,$jsonExpected): void
+    {
         $csvToJson = new CsvToJson();
         $csvToJson->setcsv($csvToParse);
-        $jsonResponse = $csvToJson->__invoke();
-
-        $jsonExpected = '[{"col1":"val1.1","col2":"val1.2"},{"col1":"val2.1","col2":"val2.2"}]';
-        $this->assertEquals($jsonExpected, $jsonResponse, "Parse JSON");
-    }
-
-    private function it_should_parse_string_num_to_ints()
-    {
-        $csvToParse = "col1;col2\n1;2\n1.1;2.2";
-
-        $csvToJson = new CsvToJson();
-        $csvToJson->setcsv($csvToParse);
-        $jsonResponse = $csvToJson->__invoke();
-
-        $jsonExpected = '[{"col1":1,"col2":2},{"col1":1.1,"col2":2.2}]';
+        $jsonResponse = $csvToJson();
+        
         $this->assertEquals($jsonExpected, $jsonResponse, "Parse int to JSON");
     }
 }
